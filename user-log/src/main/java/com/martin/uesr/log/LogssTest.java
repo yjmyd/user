16497e35cb8fccdf6a84820fe9583cc9db80e05c
yjmyd <254853183@qq.com>
2016-04-25 00:08:15 +0800

    user init log

---
+++ user-log/src/main/java/com/martin/uesr/log/LogssTest.java
@@ -0,0 +1,10 @@
+package com.martin.uesr.log;
+
+/**
+ * Created by Administrator on 2016/4/24.
+ */
+public class LogssTest {
+    public static void info(){
+        System.out.print("logs....");
+    }
+}
