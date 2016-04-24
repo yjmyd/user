ae020e6cb5ec9cd21f0f2516c46484a24fb36362
yjmyd <254853183@qq.com>
2016-04-25 00:09:53 +0800

    user init service impl

---
+++ user-service-impl/src/main/java/com/martin/user/ServiceTest.java
@@ -0,0 +1,15 @@
+package com.martin.user;
+
+import com.martin.uesr.log.LogssTest;
+
+/**
+ * Created by Administrator on 2016/4/24.
+ */
+public class ServiceTest {
+    public void serv() {
+        LogssTest.info();
+    }
+    public  void say(){
+        LogssTest.info();
+    }
+}
